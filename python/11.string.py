cd5ddd2ad386ca91604652610565f6a0d9960127
Araika <ar_tokenova@kbtu.kz>
2026-01-27 21:40:32 +0500

    strings

---
+++ python/11.string.py
@@ -0,0 +1,47 @@
+print("hello,world!")
+print("hello,world:'python'")
+
+a="you are amazing!"
+print(a[8:],"world")
+
+a="KBTU 2029!"
+print(a[-5:-1])
+
+a="my name is akbala"
+print("her name is: ",a[11:].upper())
+
+b="PYtHon-programming lanGUAGE"
+print(b.lower())
+
+c="  good day!   "
+print(c.strip())
+
+k="good day!"
+print(k.replace("g","G"))
+
+q="banana aple candy bread oil"
+print(a.split("|"))
+
+#concat
+a="my"
+b=" favourite"
+c=" food is: "
+d=input()
+print(a+b+c+d)
+
+
+age=20
+q=f"ma brother {age} years"
+print(q)
+
+d="my name is Arai \n my age is 18"
+print(d)
+
+e="i love apple,apple is my favourite fruit"
+r=e.count("a")
+print(r)
+
+t="arman arai erlan nurlan samat alima samal"
+w=t.capitalize()
+print(w)
+
