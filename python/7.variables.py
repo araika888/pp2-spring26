01da046d2c8cc8b1d3648378dcc352a9aafe04ce
Araika <ar_tokenova@kbtu.kz>
2026-01-27 19:07:10 +0500

    seven file

---
+++ python/7.variables.py
@@ -0,0 +1,45 @@
+#it's my age and name
+my_age=18
+my_name="araika"
+print(my_age)
+print(my_name)
+
+
+#assign multiple values
+x,y,z="python","good","language"
+print(x,y,z)
+print(x,z)
+
+#variables
+x="i"
+y="will"
+z="graduate"
+q="in 29"
+print(x,y,z,q)
+
+#def function
+x = "best"
+
+def myfunc():
+  x = "fantastic"
+  print("KBTU is " + x)
+
+myfunc()
+
+print("KBTU the " + x)
+
+#def function part2
+
+x="great"
+
+def point():
+  print("25 is",x,"point")
+
+point()
+
+#def function3
+def calc(x):
+  n=int(input())
+  print(n+x)
+
+calc(5)
