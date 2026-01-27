dc889ea2dbae0ea74d2defd7998b4279cffaa8d1
Araika <ar_tokenova@kbtu.kz>
2026-01-27 16:03:47 +0500

    six file name

---
+++ python/6.comments.py
@@ -0,0 +1,15 @@
+#if your code is correct print is executed
+if 4 > 2:
+    print("it's correct!")
+
+#in the terminal it shows kbtu'29
+print("kbtu'29")
+
+#but 8 is not less than 2 
+if 8 < 2:
+    print("2 is greater than 8")
+#it's true
+if 10 > 4:
+    print("4 is less than 10")
+#it's fact for name python
+print("Python was named after the British comedy show “Monty Python's Flying Circus”, not after the snake.")
