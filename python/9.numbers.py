f5e79e583be21f2b025f5a4f64fe9990d1fa5838
Araika <ar_tokenova@kbtu.kz>
2026-01-27 20:31:17 +0500

    nine file

---
+++ python/9.numbers.py
@@ -0,0 +1,35 @@
+#numbers type
+x=1
+y=0.4
+z=1j
+print(type(x))
+print(type(y))
+print(type(z))
+
+#numbers type1
+p=-1
+print(type(p))
+
+#plus
+p=2
+a=2.0
+print(p*a,type(p*a))
+
+#minus
+a=-23
+b=-67.0
+print(a-b,type(a-b))
+
+#int
+a=23
+b=2.0
+print(type(a*b))
+print(type(int(a*b)))
+
+#type
+x=1.0
+r=-34.0
+q=-765.5677
+print(type(x))
+print(type(r))
+print(type(q))
