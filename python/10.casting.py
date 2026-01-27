060cfda11c9089f2bf355752d4a79ccf7586dc06
Araika <ar_tokenova@kbtu.kz>
2026-01-27 20:40:55 +0500

    ten file name

---
+++ python/10.casting.py
@@ -0,0 +1,17 @@
+x=int(1)
+print(type(x))
+
+y=float(2)
+print(type(y))
+
+z=str(2)
+print(type(z))
+
+q=int("8")
+print(q,type(q))
+
+c=int(3.9)
+print(c,type(c))
+
+c=float("-34")
+print(c,type(c))
