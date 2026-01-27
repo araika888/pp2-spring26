049a36b5eceb42bb7629909cb3fe80d818f2ca82
Araika <ar_tokenova@kbtu.kz>
2026-01-27 15:12:05 +0500

    fours file

---
+++ python/4.syntax.py
@@ -0,0 +1,14 @@
+if 2 < 3:
+    print("3 is greater than 2")
+
+if 7 < 9:
+    print("9 is greater than 7")
+
+if 34 < 1:
+    print("34 is less than 1")
+
+print("how are you?")
+
+print("bye,guys!")
+
+print("python is fun!")
