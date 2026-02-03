045c831f0343105367a86c61d6acdf878f501a39
Araika <ar_tokenova@kbtu.kz>
2026-02-03 23:08:52 +0500

    boolean2

---
+++ practice2/boolean/02boolean_comparison.py
@@ -0,0 +1,20 @@
+#first
+print("cat"=="cat")
+print("mmm"=="nnn")
+print("hello"=="salem")
+#second
+a="my job"
+b="my life"
+if a==b:
+    print("yes")
+else:
+    print("no")
+#third
+q=8
+x=q>5
+print(x)
+#fourth
+a="my name"
+b="my uni"
+print(bool(a))
+print(bool(b))
