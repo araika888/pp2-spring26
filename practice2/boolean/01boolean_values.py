052ea140069283cec84d594ab5bb5cf71561c3e2
Araika <ar_tokenova@kbtu.kz>
2026-02-03 22:51:10 +0500

    boolean

---
+++ practice2/boolean/01boolean_values.py
@@ -0,0 +1,22 @@
+#first 
+print(9>10)
+print(8>7)
+print(10==10)
+#second
+a=200
+c=-200
+if a>c:
+    print("a greater than a")
+else:
+    print("c greater than a")
+#third
+print(bool('KBTU29'))
+print(bool(888))
+#fourth
+a="salem alem"
+b=7272
+print(bool(a))
+print(bool(b))
+
+
+
