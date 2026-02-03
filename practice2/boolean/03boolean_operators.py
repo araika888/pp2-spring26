d1921192f9a827ae686d3938bff712d3d1ee85a9
Araika <ar_tokenova@kbtu.kz>
2026-02-03 23:40:39 +0500

    boolean3

---
+++ practice2/boolean/03boolean_operators.py
@@ -0,0 +1,58 @@
+#first
+sum_1=88+77
+sum_2=sum_1+100
+print(sum_1,sum_2)
+#second
+a=12
+b=3
+c=12/3
+print("a divide by b is:",c)
+#third
+number1=10
+number2=5
+print(number1+number2)
+print(number1-number2)
+print(number1*number2)
+print(number1/number2)
+print(number1%number2)
+print(number1**number2)
+print(number1//number2)
+#fourth
+number=[1,2,3,4,5]
+if(count:=len(number))>2:
+    print(f"List has {count} number")
+#fifth
+z=100
+k=100
+print(z<=k and z>=k)
+#six
+a=188
+b=2
+if a%b==0 and a>b:
+    print("YES")
+else:
+    print("NO")
+#seven
+q=9
+print(not(q>3 or q==3))
+#eight
+fruits=["apple","banana","mango","cherry"]
+number=[1,2,3,4,5,6,7,8,9]
+q=number
+print(fruits is number)
+print(q is number)
+print(fruits is not number)
+#nine
+number=[8,9,53,4,2,32,66,67]
+print(67 in number)
+print(88 in number)
+print(67 not in number)
+#ten
+x=1010
+print(8 & 9)
+print(7 | 8)
+print(2 ^ 3)
+print(x<<2)
+print(x>>3)
+
+
