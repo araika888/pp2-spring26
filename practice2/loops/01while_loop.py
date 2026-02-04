95581c4233a3457d67fe904c4a4f7b6564cc4867
Araika <ar_tokenova@kbtu.kz>
2026-02-04 11:46:56 +0500

    while loop

---
+++ practice2/loops/01while_loop.py
@@ -0,0 +1,17 @@
+i=1
+while i < 8:
+    print(i,end=" ")
+    i+=1
+print()
+
+p=7
+while p!=1:
+    print(p,end=" ")
+    p-=1
+print()
+
+a=1
+while a < 4:
+    print(a,end=" ")
+    a+=1
+print()
