f38ce7cd99c9e32180bfd42ae7148fce1682a02f
Araika <ar_tokenova@kbtu.kz>
2026-02-04 10:56:57 +0500

    it's if_elif_else

---
+++ practice2/if_else/03if_elif_else.py
@@ -0,0 +1,26 @@
+#first
+a=200
+b=300
+c=100
+if a%c==0 and b%a==0:
+    print("YES")
+elif b%a==0 or a%c==0:
+    print("NORMAL")
+else:
+    print("NO")
+
+#second
+a=100
+if a >=100 and a % 2 == 0:
+    print("even")
+elif a < 100 :
+    print("No")
+else:
+    print("NOT")
+#third
+a=1
+b=0
+if a * b == 0:
+    print("equal is 0")
+else:
+    print("it's not equal 0")
