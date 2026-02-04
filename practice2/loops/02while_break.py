b73bd085d3bd91ab4d4bd02fafaa96ff78318069
Araika <ar_tokenova@kbtu.kz>
2026-02-04 11:52:13 +0500

    while break

---
+++ practice2/loops/02while_break.py
@@ -0,0 +1,24 @@
+i=0
+while i < 6:
+    print(i,end=" ")
+    if i == 4:
+        break
+    i += 1
+
+print()
+
+i=8
+while i > 0:
+    print(i,end=" ")
+    if i==3:
+        break
+    i -= 1
+print()
+
+i=4
+while i < 11:
+    print(i,end=" ")
+    if i == 9:
+        break
+    i += 2
+print()
