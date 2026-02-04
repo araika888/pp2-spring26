e2e425af67c28ac7a4e80d19e1a2f65ec132f69f
Araika <ar_tokenova@kbtu.kz>
2026-02-04 12:04:10 +0500

    while continue

---
+++ practice2/loops/03while_continue.py
@@ -0,0 +1,25 @@
+i=0
+while i < 9:
+    i += 1
+    if i == 5:
+        continue
+    print(i,end=" ")
+print()
+
+i=1
+while i < 12:
+    i += 2
+    if i == 7:
+        continue
+    print(i,end=" ")
+print()
+
+i=8
+while i > 1:
+    if i == 4:
+        i-=1
+        continue
+    print(i,end=" ")
+    i-=1
+
+print()
