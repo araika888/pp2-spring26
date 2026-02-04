3b03950281963bb5730fc0304cc92a261244c94b
Araika <ar_tokenova@kbtu.kz>
2026-02-04 12:08:59 +0500

    for loop

---
+++ practice2/loops/04for_loop.py
@@ -0,0 +1,14 @@
+fruits=["apple","banana","cherry","strawberry"]
+for i in fruits:
+    print(i,end=" ")
+print()
+
+number=["8","777","666","67","76"]
+for q in number:
+    print(q,end="'")
+print()
+
+alphabet=["a","b","c","d"]
+for u in alphabet:
+    print(u,end=" ")
+print()
