134c6a1ebed0d8920c3219a10270ec8eeb232fb4
Araika <ar_tokenova@kbtu.kz>
2026-02-04 10:34:06 +0500

    if_else intro

---
+++ practice2/if_else/01if_statement.py
@@ -0,0 +1,33 @@
+#first
+a=44
+b=55
+if b > a:
+    print("b greater than a")
+
+#second
+a=88
+b=90
+if b > a:
+    print("b is greater than a")
+elif b < a:
+    print("a is greater than b")
+
+#third
+score=93
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("F")
+else:
+    print("fail")
+#fourth
+a=91
+b=81
+if a+b >= 100:
+    print("it's normal great")
+else:
+    print("it's not normal great")
