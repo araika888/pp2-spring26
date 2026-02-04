e93886a8c042dd46b1e83938b7609dc92452b4d6
Araika <ar_tokenova@kbtu.kz>
2026-02-04 12:25:39 +0500

    for countinue

---
+++ practice2/loops/06for_continue.py
@@ -0,0 +1,21 @@
+name=["Aruzhan","Aiman","Aisaule","Nursaule"]
+for i in name:
+    if i == "Nursaule":
+        continue
+    print(i,end=" ")
+print()
+
+number=[1,2,3,4,5,6,867,7]
+for i in number:
+    if i == 867:
+        continue
+    print(i,end="/")
+print()
+
+alphabet=["T","O","o","K","E","N","O","V","A"]
+for a in alphabet:
+    if a == "o":
+        continue
+    print(a,end="")
+
+print()
