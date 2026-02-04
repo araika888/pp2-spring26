0c09943b4193bb9e58f72024101dc05e75efa9de
Araika <ar_tokenova@kbtu.kz>
2026-02-04 11:17:08 +0500

    short hand(if elif else)

---
+++ practice2/if_else/04short_hand_if_else.py
@@ -0,0 +1,18 @@
+#first
+a=5
+b=3
+if a > b : print("a is greater than b")
+#second
+a=8
+b=9
+print("Equal is 17") if a < b else print("Not equal 17")
+#third
+a=88
+b=90
+bigger = a if a > b else b
+print("Bigger is",bigger)
+#fourth
+val1=99
+val2=56
+max_value = val1 if val1 > val2 else val2
+print("Maximum value:",max_value)
