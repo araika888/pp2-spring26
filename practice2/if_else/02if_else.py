6501c1e83d2ab48bb9f497d9350040a3832c3665
Araika <ar_tokenova@kbtu.kz>
2026-02-04 10:42:14 +0500

    it's if_else

---
+++ practice2/if_else/02if_else.py
@@ -0,0 +1,30 @@
+#first
+a=1
+b=2
+if a*b == 2:
+    print("it's equal to 2")
+else:
+    print("it's not equal to 2")
+
+#second
+age1=10
+age2=8
+if age1+age2 == 18:
+    print("my age:18")
+else:
+    print("my age not 18")
+
+#third
+number=66
+if number % 3 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+#fourth
+temp=-20
+if temp > 0:
+    print("It's normal!")
+elif temp < 0:
+    print("It's cold!")
+else:
+    print("It's hot!!!!")
