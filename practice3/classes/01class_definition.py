6cc2a25852a7de49c8743df9b5a0d606fb2ea474
Araika <ar_tokenova@kbtu.kz>
2026-02-18 10:41:51 +0500

    01class_def

---
+++ practice3/classes/01class_definition.py
@@ -0,0 +1,22 @@
+def my_function1():
+    print("Hello from a function")
+my_function1()
+my_function1()
+
+def fahrenheit_to_celsius(fahrenheit):
+    return(fahrenheit-32) * 5/9
+print(fahrenheit_to_celsius(77))
+print(fahrenheit_to_celsius(95))
+print(fahrenheit_to_celsius(50))
+
+def get_greeting1():
+    return "Hello from a function"
+message=get_greeting1()
+print(message)
+
+def get_greeting():
+    return "Hello from a function"
+print(get_greeting())
+
+def my_function():
+    pass
